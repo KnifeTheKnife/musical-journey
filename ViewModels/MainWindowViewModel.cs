323f5c52086efb61ac838bdb0c3a4fd032eb8ed1
KnifeTheKnife <k.theknife789@gmail.com>
2025-11-21 09:26:02 +0100

    Switch to C#: Add Avalonia project template

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace musical_journey.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace musical_journey.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
