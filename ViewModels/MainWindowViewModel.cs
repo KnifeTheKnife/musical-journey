82474413f5c99eade65ce6621b8e4e38a6c271b5
KnifeTheKnife <k.theknife789@gmail.com>
2026-01-29 20:46:59 +0100

    Sidebar: clicking now brings up contents in the main bar, clicking on song brings up its tags on the right

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Avalonia.Controls;
@@ -11,16 +12,104 @@ using musical_journey.Services.Interfaces;
 
 namespace musical_journey.ViewModels;
 
+// Wrapper class to expose Song fields as properties for binding
+public class SongWrapper
+{
+    private readonly Song _song;
+    
+    public SongWrapper(Song song)
+    {
+        _song = song;
+    }
+    
+    public string Title => _song.title;
+    public string Artist => _song.artist;
+    public string Album => _song.album;
+    public string TrackNo => _song.trackNo;
+    public string Genre => _song.genre;
+    public string Date => _song.date;
+    public string DiscNo => _song.discNo;
+    public string Path => _song.path;
+    
+    public Song GetSong() => _song;
+}
+
+// Wrapper for album folders with command binding
+public class AlbumFolder
+{
+    public string Name { get; set; } = "";
+    public string Path { get; set; } = "";
+    public ICommand? ClickCommand { get; set; }
+}
+
 public class MainWindowViewModel : ViewModelBase
 {
     private readonly IFsRead fsRead;
+    private readonly IGetTags getTags;
     private Window? mainWindow;
+    private string _selectedSongTitle = "";
+    private string _selectedSongArtist = "";
+    private string _selectedSongAlbum = "";
+    private string _selectedSongTrackNo = "";
+    private string _selectedSongGenre = "";
+    private string _selectedSongDate = "";
+    private string _selectedSongDiscNo = "";
+    private string _selectedSongPath = "";
     
     public ICommand BrowseMusicFilesCommand { get; }
+
+    public string SelectedSongTitle
+    {
+        get => _selectedSongTitle;
+        set => SetProperty(ref _selectedSongTitle, value);
+    }
+
+    public string SelectedSongArtist
+    {
+        get => _selectedSongArtist;
+        set => SetProperty(ref _selectedSongArtist, value);
+    }
+
+    public string SelectedSongAlbum
+    {
+        get => _selectedSongAlbum;
+        set => SetProperty(ref _selectedSongAlbum, value);
+    }
+
+    public string SelectedSongTrackNo
+    {
+        get => _selectedSongTrackNo;
+        set => SetProperty(ref _selectedSongTrackNo, value);
+    }
+
+    public string SelectedSongGenre
+    {
+        get => _selectedSongGenre;
+        set => SetProperty(ref _selectedSongGenre, value);
+    }
+
+    public string SelectedSongDate
+    {
+        get => _selectedSongDate;
+        set => SetProperty(ref _selectedSongDate, value);
+    }
+
+    public string SelectedSongDiscNo
+    {
+        get => _selectedSongDiscNo;
+        set => SetProperty(ref _selectedSongDiscNo, value);
+    }
+
+    public string SelectedSongPath
+    {
+        get => _selectedSongPath;
+        set => SetProperty(ref _selectedSongPath, value);
+    }
 
     public MainWindowViewModel()
     {
         fsRead = new FsRead();
+        getTags = new GetTag();
         BrowseMusicFilesCommand = new AsyncCommand(BrowseAndGetMusicFiles);
     }
     
@@ -29,13 +118,38 @@ public class MainWindowViewModel : ViewModelBase
         mainWindow = window;
     }
 
-    //button counter
-    public ObservableCollection<string> Albums { get; } = new ObservableCollection<string>();
-    public ObservableCollection<string> AlbumName { get; } = new ObservableCollection<string>();
+    public ObservableCollection<AlbumFolder> AlbumFolders { get; } = new ObservableCollection<AlbumFolder>();
+    public ObservableCollection<SongWrapper> Songs { get; } = new ObservableCollection<SongWrapper>();
+    
+    private SongWrapper? _selectedSong;
+    public SongWrapper? SelectedSong
+    {
+        get => _selectedSong;
+        set
+        {
+            if (SetProperty(ref _selectedSong, value) && value != null)
+            {
+                UpdateSongInfo(value.GetSong());
+            }
+        }
+    }
+
+    private void UpdateSongInfo(Song song)
+    {
+        SelectedSongTitle = song.title;
+        SelectedSongArtist = song.artist;
+        SelectedSongAlbum = song.album;
+        SelectedSongTrackNo = song.trackNo;
+        SelectedSongGenre = song.genre;
+        SelectedSongDate = song.date;
+        SelectedSongDiscNo = song.discNo;
+        SelectedSongPath = song.path;
+    }
+
     private async Task BrowseAndGetMusicFiles()
     {
-        Albums.Clear();
-        AlbumName.Clear();
+        AlbumFolders.Clear();
+        Songs.Clear();
 
         if (mainWindow?.StorageProvider == null)
             return;
@@ -52,31 +166,62 @@ public class MainWindowViewModel : ViewModelBase
 
         var selectedPath = folders[0].Path.LocalPath;
         
-        var musicFiles = await Task.Run(() => fsRead.GetMusicFiles(selectedPath));
-        //stuff to attach to buttons
+        // Get directories
         List<string> directories = await Task.Run(() => fsRead.ScanForDirectory(selectedPath));
-        List<string> name = await Task.Run(() => fsRead.DirectoryNames(selectedPath));
-        for(int i=0;  i<directories.Count; i++)
+        List<string> names = await Task.Run(() => fsRead.DirectoryNames(selectedPath));
+        
+        if (directories.Count != names.Count)
         {
-            AlbumName.Add(name[i]);
-            Albums.Add(directories[i]);
-            
-             }
+            int count = Math.Min(directories.Count, names.Count);
 
-
-        // TODO: Do something with musicFiles
-        System.Diagnostics.Debug.WriteLine($"Found {musicFiles.Count} music files");
+            for (int i = 0; i < count; i++)
+            {
+                var dir = directories[i];
+                var nm = names[i];
+                var folder = new AlbumFolder
+                {
+                    Name = nm,
+                    Path = dir,
+                    ClickCommand = new AsyncCommand(async () => await LoadSongsFromFolder(dir))
+                };
+                AlbumFolders.Add(folder);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < directories.Count; i++)
+            {
+                var dir = directories[i];
+                var nm = names[i];
+                var folder = new AlbumFolder
+                {
+                    Name = nm,
+                    Path = dir,
+                    ClickCommand = new AsyncCommand(async () => await LoadSongsFromFolder(dir))
+                };
+                AlbumFolders.Add(folder);
+            }
+        }
     }
 
-    //soon
-    public async Task AlbumFolderFunc()
+    private async Task LoadSongsFromFolder(string folderPath)
     {
-        return;
+        Songs.Clear();
+        
+        var musicFiles = await Task.Run(() => fsRead.GetMusicFiles(folderPath));
+        
+        foreach (var file in musicFiles)
+        {
+            var song = await Task.Run(() => getTags.GetTags(file));
+            Songs.Add(new SongWrapper(song));
+        }
+        
+        System.Diagnostics.Debug.WriteLine($"Loaded {musicFiles.Count} songs from {folderPath}");
     }
    
   }
 
-// Simple async command implementation
+
 public class AsyncCommand : ICommand
 {
     private readonly Func<Task> execute;
